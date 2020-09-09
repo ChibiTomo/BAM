01c1d2566244e68773d8922f6031e264edb83ab7
ChibiTomo <thomias.yannis@gmail.com>
2020-09-09 11:08:54 +0200

    Account have historic.

--- src/main/java/net/chibidevteam/bam/Account.java
+++ src/main/java/net/chibidevteam/bam/Account.java
@@ -2,13 +2,19 @@ package net.chibidevteam.bam;
 
 import net.chibidevteam.bam.exceptions.WrongCurrencyException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
     private int balance;
     private Currency currency;
+    private List<String> historic = new ArrayList<>();
 
     public Account(int defaultBalance, Currency currency) {
         balance = defaultBalance;
         this.currency = currency;
+
+        historic.add("Creation: " + this.balance + this.currency.getSymbol());
     }
 
     public Currency getCurrency() {
@@ -19,17 +25,23 @@ public class Account {
         return balance + currency.getSymbol();
     }
 
-    public void add(int amount, Currency currency) throws WrongCurrencyException {
+    public void credit(int amount, Currency currency) throws WrongCurrencyException {
         if (!this.currency.equals(currency)) {
-            throw new WrongCurrencyException("Trying to add " + currency + " to a " + this.currency + " account.");
+            throw new WrongCurrencyException("Trying to credit " + currency + " to a " + this.currency + " account.");
         }
         balance += amount;
+        historic.add("Credit: " + amount + this.currency.getSymbol());
     }
 
-    public void retrieve(int amount, Currency currency) throws WrongCurrencyException {
+    public void debit(int amount, Currency currency) throws WrongCurrencyException {
         if (!this.currency.equals(currency)) {
-            throw new WrongCurrencyException("Trying to retrieve " + currency + " from a " + this.currency + " account.");
+            throw new WrongCurrencyException("Trying to debit " + currency + " from a " + this.currency + " account.");
         }
         balance -= amount;
+        historic.add("Debit: " + amount + this.currency.getSymbol());
+    }
+
+    public List<String> getHistoric() {
+        return historic;
     }
 }
--- src/test/java/net/chibidevteam/bam/AccountTest.java
+++ src/test/java/net/chibidevteam/bam/AccountTest.java
@@ -5,6 +5,10 @@ import net.chibidevteam.bam.exceptions.WrongCurrencyException;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class AccountTest {
@@ -44,7 +48,7 @@ public class AccountTest {
         String balance = account.getBalance();
 
         // THEN
-        String expected = "20" + Currency.DOLLAR.getSymbol();
+        String expected = "20$";
         assertThat(balance).isEqualTo(expected);
     }
 
@@ -57,22 +61,22 @@ public class AccountTest {
         String balance = account.getBalance();
 
         // THEN
-        String expected = "20" + Currency.EURO.getSymbol();
+        String expected = "20€";
         assertThat(balance).isEqualTo(expected);
     }
 
     @Test
     @SneakyThrows
-    public void should_balance_be_30_euro_after_adding_10_euro_with_starting_balance_of_20_euro() {
+    public void should_balance_be_30_euro_after_crediting_10_euro_with_starting_balance_of_20_euro() {
         // GIVEN
         Account account = new Account(20, Currency.EURO);
 
         // WHEN
-        account.add(10, Currency.EURO);
+        account.credit(10, Currency.EURO);
         String balance = account.getBalance();
 
         // THEN
-        String expected = "30" + Currency.EURO.getSymbol();
+        String expected = "30€";
         assertThat(balance).isEqualTo(expected);
     }
 
@@ -83,22 +87,22 @@ public class AccountTest {
         Account account = new Account(20, Currency.EURO);
 
         // WHEN
-        account.retrieve(10, Currency.EURO);
+        account.debit(10, Currency.EURO);
         String balance = account.getBalance();
 
         // THEN
-        String expected = "10" + Currency.EURO.getSymbol();
+        String expected = "10€";
         assertThat(balance).isEqualTo(expected);
     }
 
     @Test
-    public void should_throw_when_adding_wrong_currency() {
+    public void should_throw_when_crediting_wrong_currency() {
         // GIVEN
         Account account = new Account(20, Currency.EURO);
 
         Assertions.assertThatThrownBy(() -> {
             // WHEN
-            account.add(10, Currency.DOLLAR);
+            account.credit(10, Currency.DOLLAR);
         })
 
                 // THEN
@@ -106,17 +110,17 @@ public class AccountTest {
     }
 
     @Test
-    public void should_not_change_balance_when_adding_wrong_currency() {
+    public void should_not_change_balance_when_crediting_wrong_currency() {
         // GIVEN
         Account account = new Account(20, Currency.EURO);
 
         Assertions.catchThrowable(() -> {
             // WHEN
-            account.add(10, Currency.DOLLAR);
+            account.credit(10, Currency.DOLLAR);
         });
 
         // THEN
-        String expected = "20"+Currency.EURO.getSymbol();
+        String expected = "20€";
         assertThat(account.getBalance()).isEqualTo(expected);
     }
 
@@ -127,7 +131,7 @@ public class AccountTest {
 
         Assertions.assertThatThrownBy(() -> {
             // WHEN
-            account.retrieve(10, Currency.DOLLAR);
+            account.debit(10, Currency.DOLLAR);
         })
 
                 // THEN
@@ -141,12 +145,69 @@ public class AccountTest {
 
         Assertions.catchThrowable(() -> {
             // WHEN
-            account.retrieve(10, Currency.DOLLAR);
+            account.debit(10, Currency.DOLLAR);
         });
 
         // THEN
-        String expected = "20"+Currency.EURO.getSymbol();
+        String expected = "20€";
         assertThat(account.getBalance()).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_save_historic_on_account_creation() {
+        // GIVEN
+        Account account = new Account(20, Currency.DOLLAR);
+
+        // WHEN
+
+        // THEN
+        List<String> expected = Arrays.asList("Creation: 20$");
+        assertThat(account.getHistoric()).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    @SneakyThrows
+    public void should_save_credit_in_historic() {
+        // GIVEN
+        Account account = new Account(20, Currency.DOLLAR);
+
+        // WHEN
+        account.credit(10, Currency.DOLLAR);
+
+        // THEN
+        List<String> expected = Arrays.asList("Creation: 20$", "Credit: 10$");
+        assertThat(account.getHistoric()).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    @SneakyThrows
+    public void should_save_debit_in_historic() {
+        // GIVEN
+        Account account = new Account(20, Currency.DOLLAR);
+
+        // WHEN
+        account.debit(10, Currency.DOLLAR);
+
+        // THEN
+        List<String> expected = Arrays.asList("Creation: 20$", "Debit: 10$");
+        assertThat(account.getHistoric()).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    @SneakyThrows
+    public void should_save_all_actions_in_correct_order_in_historic() {
+        // GIVEN
+        Account account = new Account(20, Currency.DOLLAR);
+
+        // WHEN
+        account.credit(10, Currency.DOLLAR);
+        account.debit(20, Currency.DOLLAR);
+        account.credit(50, Currency.DOLLAR);
+        account.debit(15, Currency.DOLLAR);
+
+        // THEN
+        List<String> expected = Arrays.asList("Creation: 20$", "Credit: 10$", "Debit: 20$", "Credit: 50$", "Debit: 15$");
+        assertThat(account.getHistoric()).containsExactlyInAnyOrderElementsOf(expected);
     }
 
 }
